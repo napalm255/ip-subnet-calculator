61a33dd68a2ba03d70bbecbf666954fcc6f107d4
napalm255 <napalm255@gmail.com>
2017-12-20 15:47:17 -0500

    added html

--- index.py
+++ index.py
@@ -1,12 +1,65 @@
+#!/usr/bin/env python
+"""IP Subnet Calculator."""
+
+from __future__ import print_function
 import json
-import datetime
+import netaddr
+from netaddr import IPAddress, IPNetwork
 
 
 def handler(event, context):
-    data = {
-        'output': 'Hello World',
-        'timestamp': datetime.datetime.utcnow().isoformat()
-    }
-    return {'statusCode': 200,
-            'body': json.dumps(data),
-            'headers': {'Content-Type': 'application/json'}}
+    """Lambda handler."""
+    # pylint: disable=unused-argument, invalid-name
+
+    try:
+        ipaddy = event['query']
+        assert ipaddy != ''
+    except KeyError:
+        web = open('ipcalc.html', 'r')
+        return web.read()
+    except AssertionError:
+        web = open('ipcalc.html', 'r')
+        return web.read()
+
+    if ' ' in ipaddy:
+        ip_parts = ipaddy.split(' ')
+        pos1 = IPAddress(ip_parts[0])
+        pos2 = IPAddress(ip_parts[1])
+        if pos2.is_netmask():
+            ipaddy = '%s/%s' % (pos1, pos2.netmask_bits())
+    elif '/' not in ipaddy:
+        ipaddy = '%s/32' % ipaddy
+
+    try:
+        ip = IPNetwork(ipaddy)
+        ip_hosts = int(ip.size) - 2
+        hostmin = ip.cidr[1] if len(ip.cidr) > 1 else ip.cidr[-1]
+        hosts = len(ip.cidr) - 2 if len(ip.cidr) > 1 else 1
+        results = {'input': str(ipaddy),
+                   'event': event,
+                   'version': str(ip.version),
+                   'private': ip.is_private(),
+                   'reserved': ip.is_reserved(),
+                   'prefixlen': str(ip.prefixlen),
+                   'address': str(ip.ip),
+                   'network': str(ip.network),
+                   'netmask': str(ip.netmask),
+                   'wildcard': str(ip.hostmask),
+                   'hostmin': str(hostmin),
+                   'hostmax': str(ip.cidr[ip_hosts]),
+                   'broadcast': str(ip.broadcast),
+                   'hosts/net': str(hosts)}
+    except netaddr.AddrFormatError as errstr:
+        results = {'error': '%s' % errstr}
+
+    return json.dumps(results)
+
+
+if __name__ == "__main__":
+    print(handler({'query': '192.168.1.69'}, None))
+    print(handler({'query': '192.168.0.2/23'}, None))
+    print(handler({'query': '192.168.0.3 255.255.252.0'}, None))
+    print(handler({'query': '192.168.d.a'}, None))
+    print(handler({'query': '45824759'}, None))
+    print(handler({'query': ''}, None))
+    print(handler({}, None))
